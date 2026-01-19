28c9d7df5ab9e228355ffa7e23241115c01660c2
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-01-19 13:29:46 +0100

    no perder cambios

---
+++ bancoApp/src/main/java/es/banco/App.java
@@ -0,0 +1,17 @@
+package es.banco;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Faker faker = new Faker();
+    	
+        System.out.println( faker.pokemon().name() );
+    }
+}
